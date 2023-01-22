042e6d538a3529cab6dfcbe9a248d46e0dda14b1
serhiialpatov <alpatovsn@gmail.com>
2023-01-22 14:25:57 +0200

    css update

--- app/routes.py
+++ app/routes.py
@@ -3,12 +3,15 @@ from flask import render_template
 from app import forms
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def sign_in():  # put application's code here
     form = forms.LoginForms()
     return render_template('index.html', form=form)
-@app.route('/sign_up')
+
+
+@app.route('/sign_up', methods=['POST', 'GET'])
 def sign_up():
     form = forms.RegistrationForm()
+    if form.validate_on_submit():
+        print(form.data)
     return render_template('registration.html', form=form)
-
