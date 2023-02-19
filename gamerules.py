c156e36deb5a1a1b36be51356a83d094025a8a6e
serhiialpatov <alpatovsn@gmail.com>
2023-02-19 14:37:02 +0200

    фыаф

--- gamerules.py
+++ gamerules.py
@@ -3,11 +3,32 @@ import random
 import db_controller
 
 
-def get_word():
-    data = db_controller.Words().get_all()
-    return random.choice(data)[1]
+class Game:
+    letters_in_game = []
+
+    def __init__(self):
+        self.data = random.choice(db_controller.Words().get_all())[1]
+
+    def set_mask(self):
+        return '*' * self.data.__len__()
+
+    def update_mask(self):
+        word = ""
+        for x in self.data:
+            if x in self.letters_in_game:
+                word += x
+            else:
+                word += '*'
+        return word
+
+    def check_if_letter(self, letter):
+        self.letters_in_game.append(letter)
 
 
-def set_mask(word: str):
-    return '*' * word.__len__()
+obj = Game()
+while True:
 
+    obj.check_if_letter(input('>'))
+    print(obj.update_mask())
+    if obj.update_mask().count('*') == 0:
+        break
